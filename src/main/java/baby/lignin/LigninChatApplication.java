405d3ddac388d478eb7f1e43904cea9482a7d282
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-16 23:35:14 +0900

    initial commit

---
+++ src/main/java/baby/lignin/LigninChatApplication.java
@@ -0,0 +1,13 @@
+package baby.lignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigninChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigninChatApplication.class, args);
+	}
+
+}
