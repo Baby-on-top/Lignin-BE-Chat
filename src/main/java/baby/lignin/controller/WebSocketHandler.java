d29a83afbd214ef25c1a42720026f8da80a81d8a
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-17 16:31:04 +0900

    Feat : WebSocketHandler #1
    - 채팅 메시지를 읽어서 객체로 만들고 해당 room에 전송한다.

--- src/main/java/baby/lignin/controller/WebSocketHandler.java
+++ src/main/java/baby/lignin/controller/WebSocketHandler.java
@@ -1,5 +1,10 @@
 package baby.lignin.controller;
 
+import baby.lignin.model.ChatMessage;
+import baby.lignin.model.ChatRoom;
+import baby.lignin.service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -7,13 +12,18 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}", payload);
-        TextMessage textMessage = new TextMessage("Chat Server");
-        session.sendMessage(textMessage);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
     }
 }
