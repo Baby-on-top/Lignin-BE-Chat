9099c1cccd51e12afbd1670060fa326e1e083f29
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-20 03:25:19 +0900

    Feat : 채팅방 repository 추가 #3

---
+++ src/main/java/baby/lignin/repository/ChatRoomRepository.java
@@ -0,0 +1,35 @@
+package baby.lignin.repository;
+
+import baby.lignin.model.ChatRoom;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Repository
+public class ChatRoomRepository {
+    // TODO : DB 연결하여 수정할 것
+    private Map<String, ChatRoom> chatRoomMap;
+
+    // 의존관계 주입완료되면 실행되는 코드
+    @PostConstruct
+    private void init() {
+        chatRoomMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        List<ChatRoom> chatRooms = new ArrayList<>(chatRoomMap.values());
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
