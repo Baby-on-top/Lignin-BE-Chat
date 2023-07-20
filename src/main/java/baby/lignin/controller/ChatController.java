0ea530ff46bb8d28b07f66e328736a91861999b3
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-21 02:12:07 +0900

    Feat : 채팅방 입장 시, 채팅방(topic)에서 대화가 가능하도록 추가 #5

--- src/main/java/baby/lignin/controller/ChatController.java
+++ src/main/java/baby/lignin/controller/ChatController.java
@@ -1,22 +1,25 @@
 package baby.lignin.controller;
 
 import baby.lignin.model.ChatMessage;
+import baby.lignin.service.ChatService;
+import baby.lignin.service.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 // TODO : @RestController 수정
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatService chatService;
 
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
         if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatService.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다");
         }
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        redisPublisher.publish(chatService.sendMessage(message), message);
     }
 }
