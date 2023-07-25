3d45b66eb554daad0602a712449186d59598ea61
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-25 10:58:39 +0900

    Chores : Redis 연결 코드 주석 처리 #8

--- src/main/java/baby/lignin/service/RedisPublisher.java
+++ src/main/java/baby/lignin/service/RedisPublisher.java
@@ -1,20 +1,17 @@
 package baby.lignin.service;
 
-import baby.lignin.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
-@Service
+//@Service
 public class RedisPublisher {
     // 메시지를 Redis 서버로 발행한다.
     // 설정한 redisTemplate을 주입하고 발행을 위한 메서드를 구현한다.
     // topic에 구독자(subscriber)가 있는 경우, 발행자(publisher)가 메시지를 발행하면 구독자에게 메시지가 전달된다.
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    public void publish(ChannelTopic topic, ChatMessage message) {
-        redisTemplate.convertAndSend(topic.getTopic(), message);
-    }
+//    private final RedisTemplate<String, Object> redisTemplate;
+//
+//    public void publish(ChannelTopic topic, ChatMessage message) {
+//        redisTemplate.convertAndSend(topic.getTopic(), message);
+//    }
 }
--- src/main/java/baby/lignin/service/RedisSubscriber.java
+++ src/main/java/baby/lignin/service/RedisSubscriber.java
@@ -1,36 +1,36 @@
-package baby.lignin.service;
-
-import baby.lignin.model.ChatMessage;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.stereotype.Service;
-
-@Slf4j
-@RequiredArgsConstructor
-@Service
-public class RedisSubscriber implements MessageListener {
-    private final ObjectMapper objectMapper;
-    private final RedisTemplate redisTemplate;
-    private final SimpMessageSendingOperations messagingTemplate;
-
-    // Redis에서 메시지가 발행(publish)되면 대기하고 있던 onMessage가 해당 메시지를 받아 처리한다.
-    // 리스너에게 수신된 메시지를 비즈니스 로직을 통해 messagingTemplate을 이용해 WebSocket 구독자들에게 전달한다.
-    @Override
-    public void onMessage(Message message, byte[] pattern) {
-        try {
-            // redis에서 발행된 데이터를 받아 deserialize
-            String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
-            // ChatMessage 객체로 맵핑
-            ChatMessage roomMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
-            // WebSocket 구독자에게 채팅 메시지 전달
-            messagingTemplate.convertAndSend("/sub/chat/room/" + roomMessage.getRoomId(), roomMessage);
-        } catch (Exception e) { // TODO : Exception 처리하기
-            log.error(e.getMessage());
-        }
-    }
-}
+//package baby.lignin.service;
+//
+//import baby.lignin.model.ChatMessage;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.redis.connection.Message;
+//import org.springframework.data.redis.connection.MessageListener;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.messaging.simp.SimpMessageSendingOperations;
+//import org.springframework.stereotype.Service;
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@Service
+//public class RedisSubscriber implements MessageListener {
+////    private final ObjectMapper objectMapper;
+////    private final RedisTemplate redisTemplate;
+////    private final SimpMessageSendingOperations messagingTemplate;
+////
+////    // Redis에서 메시지가 발행(publish)되면 대기하고 있던 onMessage가 해당 메시지를 받아 처리한다.
+////    // 리스너에게 수신된 메시지를 비즈니스 로직을 통해 messagingTemplate을 이용해 WebSocket 구독자들에게 전달한다.
+////    @Override
+////    public void onMessage(Message message, byte[] pattern) {
+////        try {
+////            // redis에서 발행된 데이터를 받아 deserialize
+////            String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+////            // ChatMessage 객체로 맵핑
+////            ChatMessage roomMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
+////            // WebSocket 구독자에게 채팅 메시지 전달
+////            messagingTemplate.convertAndSend("/sub/chat/room/" + roomMessage.getRoomId(), roomMessage);
+////        } catch (Exception e) { // TODO : Exception 처리하기
+////            log.error(e.getMessage());
+////        }
+////    }
+//}
