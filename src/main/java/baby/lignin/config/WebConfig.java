1c8fbb186d206236af0dbe32cc64b6d731bc51b2
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-06 21:36:29 +0900

    Feat : CORS 에러 수정

---
+++ src/main/java/baby/lignin/config/WebConfig.java
@@ -0,0 +1,19 @@
+package baby.lignin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers", "Token")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH");
+    }
+}
